cf6deffb4d1ab81ca2436bc067c404858e6960bd
marinadev <maryaleks@mail.ru>
2020-03-16 00:36:48 +0300

    add Напишите функцию modify_list(l), которая принимает на вход список целых чисел, удаляет из него все нечётные значения,а чётные нацело делит на два. Функция не должна ничего возвращать, требуется только изменение переданного списка

--- modifyListF.py
+++ modifyListF.py
@@ -1,6 +1,9 @@
-def modify_list(*l):
-    modify_list = []
-    for x in l:
-        if x % 2 == 0:
-            modify_list.append(x // 2)
+#Напишите функцию modify_list(l), которая принимает на вход список целых чисел, удаляет из него все нечётные значения,
+# а чётные нацело делит на два. Функция не должна ничего возвращать, требуется только изменение переданного списка
+def modify_list(l): l[:] = [(x // 2) for x in l if x  % 2 == 0]
 
+
+listOne = [1, 2, 3, 4]
+print(listOne)
+modify_list(listOne)
+print(listOne)
